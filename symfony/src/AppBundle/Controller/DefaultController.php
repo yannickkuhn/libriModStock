b07e8225ad242ca5bb817faee30103af99153fb6
Yannick KUHN <yk@2dcom.fr>
2017-12-01 10:11:50 +0100

    swiftmailer ne fonctionne pas pour l'instant

--- symfony/src/AppBundle/Controller/DefaultController.php
+++ symfony/src/AppBundle/Controller/DefaultController.php
@@ -20,36 +20,29 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-
-    	/*$mailer = $this->get('mailer');
-    	$message = \Swift_Message::newInstance()
-            ->setSubject('[SITE INTERNET] - Synchronisation des commandes à '.date('d/m/Y à H:i:s'))
-            ->setFrom('yk@2dcom.fr')
-            ->setTo('yk@2dcom.fr')
-            ->setBody("Bonjour,\r\n\r\n"."Aucune commande en attente de téléchargement !\r\nA bientôt sur librairiezenobi.com");
-        $retour = $mailer->send($message);*/
-
         $this->send_mail();
-
         return $this->render('main/homepage.html.twig');
-
     }
 
-    private function send_mail($mail = null, $sujet = null, $message = null, $header = null)
+    private function send_mail($mail = null, $sujet = null, $message_txt = null, $header = null)
     {
-        $mail = 'yk@2dcom.fr'; 
-        if (!preg_match("#^[a-z0-9._-]+@(hotmail|live|msn).[a-z]{2,4}$#", $mail)) {
+        if($mail == null)
+            $mail = 'yk@2dcom.fr'; 
+        if($sujet == null)
+            $sujet = "Test depuis le default controller ... !";
+        if($message_txt == null)
+            $message_txt = "Salut à tous, voici un e-mail envoyé par un script PHP.";
+        
+        if (!preg_match("#^[a-z0-9._-]+@(hotmail|live|msn).[a-z]{2,4}$#", $mail))
             $passage_ligne = "\r\n";
-        }
-        else {
+        else
             $passage_ligne = "\n";
-        }
-        $message_txt = "Salut à tous, voici un e-mail envoyé par un script PHP.";
-        $message_html = "<html><head></head><body><b>Salut à tous</b>, voici un e-mail envoyé par un <i>script PHP</i>.</body></html>";
+
+        $message_html = "<html><head></head><body>".$message_txt."</body></html>";
         $boundary = "-----=".md5(rand());
-        $sujet = "Test depuis le default controller ... !";
-        $header = "From: \"WeaponsB\"<weaponsb@mail.fr>".$passage_ligne;
-        $header.= "Reply-to: \"WeaponsB\" <yk@2dcom.fr>".$passage_ligne;
+        
+        $header = "From: \"Librairie Zenobi\"<admin@librairiezenobi.com>".$passage_ligne;
+        $header.= "Reply-to: \"Librairie Zenobi\" <admin@librairiezenobi.com>".$passage_ligne;
         $header.= "MIME-Version: 1.0".$passage_ligne;
         $header.= "Content-Type: multipart/alternative;".$passage_ligne." boundary=\"$boundary\"".$passage_ligne;
         $message = $passage_ligne."--".$boundary.$passage_ligne;
