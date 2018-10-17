6e2167cae35c0b993e1f7520415c33aad9963910
Yannick KUHN <yk@2dcom.fr>
2018-10-17 14:17:20 +0200

    Script de suppression des images inutilisées dans le script de nettoyage

--- ddc/nettoyagebase.php
+++ ddc/nettoyagebase.php
@@ -30,6 +30,7 @@
 
 	$metaDeleted = 0;
 	$postDeleted = 0;
+	$images_utilisees_en_ligne = [];
 
 	$response = $db->query("SELECT * FROM `wpmk_posts` WHERE `post_type`='product' AND `post_title`='produit'");
 	while ($data = $response->fetch()) {
@@ -48,34 +49,21 @@
 	// 1- Supprimer les images !
 	// =======================================
 
-	echo ("SUPPRESSION DES IMAGES !<br>");
+	echo ("SUPPRESSION DES IMAGES NON UTILISEES SUR LE DISQUE !<br>");
 
 	// PARTIE 1
 
-	$response = $db->query("SELECT * FROM `wpmk_posts` WHERE (`post_name` LIKE 'image-%' OR `post_name` REGEXP '[0-9]{13}') AND (`post_mime_type`='image/jpeg' OR `post_mime_type`='image/png')");
+	$response = $db->query("SELECT * FROM `wpmk_posts` WHERE (`post_name` LIKE 'image-%' OR `post_title` REGEXP '[0-9]{13}') AND (`post_mime_type`='image/jpeg' OR `post_mime_type`='image/png')");
 	while ($data = $response->fetch()) {
-		$res = $db->prepare("DELETE FROM wpmk_postmeta WHERE post_id = '".$data['ID']."'");
-		$res->execute();
-		$metaDeleted += $res->rowCount();
+		var_dump($data['guid']);
+		$data['guid'] = pathinfo($data['guid']);
+		$data['guid'] = $data['guid']['dirname'].'/'.$data['guid']['filename'];
+		$images_utilisees_en_ligne[$data['ID']] = $data['guid'];
 	}
-	$res = $db->prepare("DELETE FROM `wpmk_posts` WHERE (`post_name` LIKE 'image-%' OR `post_name` REGEXP '[0-9]{13}') AND (`post_mime_type`='image/jpeg' OR `post_mime_type`='image/png')");
-	$res->execute();
-	$postDeleted += $res->rowCount();
 
-	// PARTIE 2
+	echo " Suppression des images non utilisées en cours ...<br>";
 
-	$response = $db->query("SELECT * FROM `wpmk_posts` WHERE `post_title` = '' AND (`post_mime_type`='image/jpeg' OR `post_mime_type`='image/png')");
-	while ($data = $response->fetch()) {
-		$res = $db->prepare("DELETE FROM wpmk_postmeta WHERE post_id = '".$data['ID']."'");
-		$res->execute();
-		$metaDeleted += $res->rowCount();
-	}
-	$res = $db->prepare("DELETE FROM `wpmk_posts` WHERE `post_title` = '' AND (`post_mime_type`='image/jpeg' OR `post_mime_type`='image/png')");
-	$res->execute();
-	$postDeleted += $res->rowCount();
-
-	echo "$metaDeleted enregistrements méta données supprimées !<br>";
-	echo "$postDeleted posts supprimées !<br>";
+	var_dump($images_utilisees_en_ligne);
 
 	// UNLINK IMAGES
 
@@ -104,9 +92,19 @@
 			if ($handle = opendir($folder)) {
 				while (false !== ($entry = readdir($handle))) {
 					if ($entry != "." && $entry != "..") {
-						if(preg_match("/image-(.*).jpeg/", $entry, $output_array)) {
-							unlink($folder.'/'.$entry);
-							$nbImagesDel++;
+						$file_server = $folder.'/'.$entry;
+						$file = str_replace("../", "", $file_server);
+						$file = str_replace("/", "\/", $file);
+						var_dump($file);
+						if(preg_grep("/$file/i", $images_utilisees_en_ligne)) {
+							var_dump('image utilisée en ligne !');
+						} else {
+							// Si le fichier ne matche pas les entrées dans la base, il faut tester qu'il s'agit d'une image 
+							// ajoutée via le web service
+							if(preg_match("/image-(.*)-/", $entry, $output_array)) {
+								unlink($folder.'/'.$entry);
+								$nbImagesDel++;
+							}
 						}
 					}
 				}
@@ -115,5 +113,8 @@
 		}
 	}
 	echo "$nbImagesDel images supprimées !<br>";
+
+	// TODO : LE SCRIPT SUPPRIME LES IMAGES THUMBNAILS ...
+
 
 
